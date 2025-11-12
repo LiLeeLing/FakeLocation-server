feaa3fe1235c4d186ed2ecdb534fc442c746a026
Ling <158540825+LiLeeLing@users.noreply.github.com>
2025-11-12 10:59:21 +0800

    Update Logger.js

--- Logger.js
+++ Logger.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
-const fs = require('fs');
-const path = require("path");
+// const fs = require('fs'); // 注释掉
+// const path = require("path"); // 注释掉
 
 Date.prototype.format = function(fmt) { 
     let o = { 
@@ -23,25 +23,27 @@ Date.prototype.format = function(fmt) {
    return fmt; 
 }
 
-const LogFileName = new Date().format("yyyy-MM-dd") + ".txt";
+// const LogFileName = new Date().format("yyyy-MM-dd") + ".txt"; // 注释掉
 
+/*  // 整块注释掉
 if(!fs.existsSync("Logs")){
     fs.mkdirSync("Logs");
 }
+*/
 
 function LogError(str){
     console.log(chalk.redBright("[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Error] ") + str);
-    fs.appendFileSync(path.join(__dirname,"./Logs/" + LogFileName), "[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Error] " + str + "\n");
+    // fs.appendFileSync(path.join(__dirname,"./Logs/" + LogFileName), "[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Error] " + str + "\n"); // 注释掉
 }
 
 function LogInfo(str){
     console.log(chalk.green("[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Info] ") + str);
-    fs.appendFileSync(path.join(__dirname,"./Logs/" + LogFileName), "[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Info] " + str + "\n");
+    // fs.appendFileSync(path.join(__dirname,"./Logs/" + LogFileName), "[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Info] " + str + "\n"); // 注释掉
 }
 
 function LogWarn(str){
     console.log(chalk.yellow("[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Warn] ") + str);
-    fs.appendFileSync(path.join(__dirname,"./Logs/" + LogFileName), "[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Warn] " + str + "\n");
+    // fs.appendFileSync(path.join(__dirname,"./Logs/" + LogFileName), "[" + new Date().format("yyyy-MM-dd hh:mm:ss") + "][Warn] " + str + "\n"); // 注释掉
 }
 
 module.exports = { LogInfo,LogWarn,LogError };
